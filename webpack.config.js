ebbd94e48a60d71347a3152056de122de28500fe
PEI-YUN LEE <may1092200258@gmail.com>
2019-04-05 15:40:15 +0800

    image

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,33 @@ module.exports = {
       filename: 'bundle.js',
       publicPath: './dist/'
    },
+   module:{
+      rules:[
+         {
+            test: /\.(js)$/,
+            exclude: /(node_modules)/,
+            use: {
+               loader: 'babel-loader',
+               options: {
+                  presets: ['@babel/preset-env']
+               }
+            }
+         },
+         {
+            test: /\.(jpe?g|png|gif|svg)$/,
+            use: [
+               {
+                  loader: 'url-loader',
+                  options: {
+                     limit: 40000,
+                     outputPath: './images'
+                  }
+               },
+               'image-webpack-loader'
+            ]
+         }
+      ]
+   }
 }
 
 
