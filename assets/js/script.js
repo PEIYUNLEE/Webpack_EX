6c08963fd0a96e85db1ea10893fd1d741b4ee1d0
PEI-YUN LEE <may1092200258@gmail.com>
2019-04-05 15:16:59 +0800

    js

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+let $ = require('jquery');
+$(document).ready(function () {
+	// when a hanburger menu is clicked
+	let $ul = $(".nav__list"),
+		 $menu = $(".nav__menu");
+
+	$($menu).click(function () {
+		// toggle menu-click Class
+		$ul.toggleClass("menu-click");
+	}); // end click event handler
+});
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+let path = require('path');
+
+module.exports = {
+   entry: './assets/js/script.js',
+   output: {
+      path: path.join(__dirname, './dist'),
+      filename: 'bundle.js',
+      publicPath: './dist/'
+   },
+}
+
+
