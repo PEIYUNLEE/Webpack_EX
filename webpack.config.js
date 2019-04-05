9f783bb8036c0b141e112359016318a9b705d010
PEI-YUN LEE <may1092200258@gmail.com>
2019-04-05 18:08:04 +0800

    gh

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
                   options: {
                      limit: 40000,
                      outputPath: './images',
-                     publicPath: '../dist/images'
+                     publicPath: './images'
                   }
                },
                'image-webpack-loader'
